03284bfd6e5f69b1f40d28babe6ddef5fde2ffdd
GilbertoTADS <gilbertotads@gmail.com>
2021-07-15 09:23:56 -0300

    ensure return currency date in DateValidator.currencyDate()

--- test/unit/DateValidator.test.ts
+++ test/unit/DateValidator.test.ts
@@ -17,7 +17,23 @@ describe('Class DateValidator', () => {
     })
     test('Must return false if parts are not numbers', () => {
         const res:boolean = date.isDate('dd/mm/aaaa');
+        const res2:boolean = date.isDate(' /  /    ');
+        const res3:boolean = date.isDate('../../....');
+        const res4:boolean = date.isDate('@@/@@/@@@@');
+        const res5:boolean = date.isDate('d6vf5/d8fgws)-3/w7+w09fg0r9g');
         expect(false).toBe(res);
+        expect(false).toBe(res2);
+        expect(false).toBe(res3);
+        expect(false).toBe(res4);
+        expect(false).toBe(res5);
+    })
+    test('Must return currency date', () => {
+        const dateSpy = new Date();
+        const day = dateSpy.getDate().toString().padStart(2,'0');
+        const year = dateSpy.getFullYear().toString().padStart(2,'0');
+        const month = (dateSpy.getMonth()+1).toString().padStart(2,'0');
+        const today = date.currencyDate();
+        expect(`${year}-${month}-${day}`).toBe(today);
     })
 
 
