e7d4edc353a6d85c07a57de53b599af1a600c3b8
GilbertoTADS <gilbertotads@gmail.com>
2021-07-15 19:39:55 -0300

    CPF Validator - initial

---
+++ src/CPF/index.ts
@@ -0,0 +1,7 @@
+import { FactoryCPFValidator } from './FactoryCPFValidator';
+import { CPFValidator } from './CPFValidator';
+
+export {
+  FactoryCPFValidator,
+  CPFValidator
+}
--- test/unit/CPFValidator.test.ts
+++ test/unit/CPFValidator.test.ts
@@ -1,33 +1,5 @@
-export class CPFValidator{
-  isCPF(cpf:string):boolean{
-    if(!cpf) return false;
-    return /[\d]{3}\.[\d]{3}\.[\d]{3}\-[\d]{2}/.test(cpf);      
-  }
-  isValid(cpf:string):boolean{
-    if(!cpf) return false;
-    cpf = cpf.replace(/[^\d]/g,'')
-    let Sum;
-    let Rest;
-    Sum = 0;
-    if (cpf == "00000000000") return false;
-    for(let i=1; i<=9; i++) 
-      Sum = Sum + parseInt(cpf.substring(i-1, i)) * (11 - i);
-    Rest = (Sum * 10) % 11;
-    if ((Rest == 10) || (Rest == 11)) 
-      Rest = 0;
-    if (Rest != parseInt(cpf.substring(9, 10)) ) 
-      return false;
-    Sum = 0;
-    for (let i = 1; i <= 10; i++) 
-      Sum = Sum + parseInt(cpf.substring(i-1, i)) * (12 - i);
-    Rest = (Sum * 10) % 11;
-    if ((Rest == 10) || (Rest == 11)) 
-      Rest = 0;
-    if (Rest != parseInt(cpf.substring(10, 11) ) ) 
-      return false;
-    return true;
-  }
-}
+//import { Factory } from './../
+
 describe('Class CPFValidator', () => {
   test('Must return true if data is CPF valid', () => {
     const cpfValidator = new CPFValidator();
